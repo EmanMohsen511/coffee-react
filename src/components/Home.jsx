63e26257c4553314cf82f8b5e70b3cd16c7c5eda
eman <emanmohsen511@gmail.com>
2023-06-14 07:58:49 +0300

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -16,15 +16,17 @@ function Home({coffee}) {
         <p>Start your day with
           a black Coffee</p>
         <button className="btn btn-light btn-lg rounded-5 w-25 " type="submit">Buy Now</button>
+    
       </div>{/* col */}
     </div>{/* row */}
   </div>{/* container */}
+        <Video/>
+    <CardList coffee ={coffee}/>
+    <Images coffee ={coffee}/>
+    <Rates />
+    <Life/>
 </section>
-<Video/>
-<CardList coffee ={coffee}/>
-<Images coffee ={coffee}/>
-<Rates />
-<Life/>
+
 
     </>
   )
